81391d4728ac2886b537f7e7b8b49baca8ccd074
DarkDrago89 <agillukman78@gmail.com>
2025-12-14 07:12:29 +0700

    Update forgot_password.php

--- api/auth/forgot_password.php
+++ api/auth/forgot_password.php
@@ -6,6 +6,7 @@ header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Methods: POST');
 header('Access-Control-Allow-Headers: Content-Type');
 
+require_once __DIR__ . '/../../includes/mailer.php';
 require_once __DIR__ . '/../../includes/functions.php';
 
 if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
